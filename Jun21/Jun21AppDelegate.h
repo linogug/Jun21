7fb7d3cf7c296411d08889bcc37ebe4689f5920f
Lino Guglielmo <linogug@aol.com>
2012-06-18 21:34:06 -0400

    Initial Commit

---
+++ Jun21/Jun21AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jun21AppDelegate.h
+//  Jun21
+//
+//  Created by Lino Guglielmo on 6/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jun21AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
